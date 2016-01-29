fb0c0c1299d417b140b54ba4429c738194445232
snowserver <justin_snow@pennunited.com>
2016-01-29 10:07:47 -0500

    syntax fix

--- lib/SnowWeather/conditions.rb
+++ lib/SnowWeather/conditions.rb
@@ -4,7 +4,7 @@ module SnowWeather
 
     def self.getConditions(state, city)
       @slash = '/'
-      @base_url 'http://api.wunderground.com/api/' + @@api_key.to_s + '/conditions/q/' + state.to_s + @slash.to_s + city.to_s + '.json'  
+      @base_url = 'http://api.wunderground.com/api/' + @@api_key.to_s + '/conditions/q/' + state.to_s + @slash.to_s + city.to_s + '.json'  
       @data = open(@base_url.to_s).read
       @parse_data = JSON.parse(@data)
       return @parse_data
