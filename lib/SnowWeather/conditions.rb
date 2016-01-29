b1249c73946638ad2f63841f2d288ebfa1d714b8
snowserver <justin_snow@pennunited.com>
2016-01-29 10:05:18 -0500

    fixed base url

--- lib/SnowWeather/conditions.rb
+++ lib/SnowWeather/conditions.rb
@@ -3,7 +3,8 @@ module SnowWeather
     include SnowWeather
 
     def self.getConditions(state, city)
-      @base_url 'http://api.wunderground.com/api/' + @@api_key.to_s + '/conditions/q/' + state.to_s + '/' + city.to_s + '.json'  
+      @slash = '/'
+      @base_url 'http://api.wunderground.com/api/' + @@api_key.to_s + '/conditions/q/' + state.to_s + @slash.to_s + city.to_s + '.json'  
       @data = open(@base_url.to_s).read
       @parse_data = JSON.parse(@data)
       return @parse_data
